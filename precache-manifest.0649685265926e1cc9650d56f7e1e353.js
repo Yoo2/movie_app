d0d71855aa401100f559dba1c00c29028803a718
Kang Seokhyun <yoo2yoo2yoo2@gmail.com>
2019-11-11 16:39:40 +0900

    Updates

---
+++ precache-manifest.0649685265926e1cc9650d56f7e1e353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8eb47110edee07b768554ad50ecf45e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5a33f9b99a7ec8a9a9b5",
+    "url": "/movie_app/static/css/main.32519d1b.chunk.css"
+  },
+  {
+    "revision": "f749c2700bb4649254ec",
+    "url": "/movie_app/static/js/2.6c3d5c7d.chunk.js"
+  },
+  {
+    "revision": "5a33f9b99a7ec8a9a9b5",
+    "url": "/movie_app/static/js/main.5bca6966.chunk.js"
+  },
+  {
+    "revision": "a5520b079f3f6e5da818",
+    "url": "/movie_app/static/js/runtime-main.08558f25.js"
+  }
+]);
